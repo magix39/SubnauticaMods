d22b4e76cf53081d37283aa09d9586903db110d9
magix39 <magix39@users.noreply.github.com>
2021-05-25 22:50:02 -0400

    1st commit
    1st commit

---
+++ MainPatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using HarmonyLib;
+namespace ScannerRoomBeacons
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            Harmony harmony = new Harmony("com.magix39.subnautica.ScannerRoomBeacons.mod");   // DO I really need this long mf name? Who cares.
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ UpdateBeaconBlips.cs
@@ -0,0 +1,86 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace ScannerRoomBeacons  // This is usually the name of your mod.
+{
+    [HarmonyPatch]
+    internal class MapRoomFunctionality_UpdateCameraBlips_Patch
+    {
+        /* Using the setup for beaconBlipRoot and beaconBlips here makes scanner map blips disappear earlier than wanted.
+         * As in the blip shows up, disappears for a while (varies depending on if a resource scan is going or not), then comes back.
+         * How long it shows depends as well on... something, idk yet. 
+         * Probably has something to do with not caching or.. something..
+         */
+
+        public static GameObject beaconBlipRoot;
+        public static List<GameObject> beaconBlips = new List<GameObject>();
+
+        /* Doing this patch after/before UpdateCameraBlips() or UpdateBlips() is best. 
+         * This isn't actually editing those functions just piggy backing off WHEN they run. 
+         */
+        [HarmonyPatch(typeof(MapRoomFunctionality))]
+        [HarmonyPatch("UpdateBlips")] 
+        [HarmonyPostfix]
+        private static void UpdateBeaconBlips(MapRoomFunctionality __instance)
+        {
+            /* Using this setup for beaconBlipRoot and beaconBlips makes scanner map blips NEVER disappear.
+             * As in the blips keep stacking each update.
+             * Forever. Every few seconds. Probably based off when the scan updates.
+             */
+            //GameObject beaconBlipRoot = new GameObject("BeaconBlipRoot");
+            //List<GameObject> beaconBlips = new List<GameObject>();
+
+            /* This block is taken from StartScanning() for some reason.
+             * It did help clear up for a while old blips and is probably needed to be placed somewhere else.
+             * It'll be needed if I don't come up with a way to check if a beaconBlip is already added and instead
+             * am just clearing the blips each time. Is it inefficient, hell yea. Do I know a better way yet, hell nah.
+             */
+            beaconBlips.Clear();
+            UnityEngine.Object.Destroy(beaconBlipRoot);
+            beaconBlipRoot = new GameObject("BeaconBlipRoot");
+            beaconBlipRoot.transform.SetParent(__instance.wireFrameWorld, false);
+
+            /*  BeaconManager.beacons is a hashset and I could probably have left it but I don't know how to
+             *  get the .transform.position using a hashset...
+             *  Also down the road I'll probably need a separate array to tack if a beacon is added to a holo map or not.
+             */
+            List<Beacon> beaconList = BeaconManager.beacons.ToList();
+
+            /* This code is based heavily on UpdateCameraBlips() because I can. It made sense at the time.*/
+            float scanRange = __instance.GetScanRange();
+            float num = scanRange * scanRange;
+            Vector3 position = beaconBlipRoot.transform.position;
+            int num2 = 0;
+            for (int i = 0; i < BeaconManager.GetCount(); i++)
+            {
+                Vector3 position2 = beaconList[i].transform.position;
+                if ((__instance.wireFrameWorld.position - position2).sqrMagnitude <= num)
+                {
+                    Vector3 vector = (position2 - position) * __instance.mapScale;
+                    if (num2 >= beaconBlips.Count)
+                    {
+                        GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(__instance.blipPrefab, vector, Quaternion.identity);
+                        gameObject.transform.SetParent(beaconBlipRoot.transform, false);
+                        beaconBlips.Add(gameObject);
+
+                        /* "Borrowed from AutosortLockers to change the color
+                         * It would be cool later on to make the color dependant on the beacon set color but idk how.
+                         */
+                        MeshRenderer[] componentsInChildren = gameObject.GetComponentsInChildren<MeshRenderer>();
+                        for (int k = 0; k < componentsInChildren.Length; k++)
+                        { componentsInChildren[k].material.color = new Color(1f, 1f, 1f, 0.5f); }
+                    }
+                    beaconBlips[num2].transform.localPosition = vector;
+                    beaconBlips[num2].SetActive(true); // Why this needed tho; possible prevent relooping something that hasn't moved?
+                    num2++;
+                }
+            }
+            for (int j = num2; j < beaconBlips.Count; j++)
+            {
+                beaconBlips[j].SetActive(false); // Why this needed tho; possible prevent relooping something that hasn't moved?
+            }
+        }
+    }
+}
